07d5f272b0c0983f99f7f1fdf399ffd7b1d1ef13
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-02-22 12:23:50 +0000

    Add updated on

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 def __str__(self):
     return f"{self.firstname} {self.lastname}"   
