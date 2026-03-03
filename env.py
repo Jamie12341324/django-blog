7eadc07816bc67ba91f53a3e7b8825e0e2326409
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-03-03 13:28:07 +0000

    remove env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-#os.environ.setdefault("DATABASE_URL","postgresql://neondb_owner:npg_emW80UYXuJRi@ep-mute-breeze-alnr2bha.c-3.eu-central-1.aws.neon.tech/issue_eject_yeast_503753")
-os.environ.setdefault("DATABASE_URL","postgresql://neondb_owner:npg_emW80UYXuJRi@ep-mute-breeze-alnr2bha.c-3.eu-central-1.aws.neon.tech/blog")
-os.environ.setdefault("SECRET_KEY", "123")
