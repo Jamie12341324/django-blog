94a984bd18961c0da48a49d45d04939d736da670
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-03-07 12:07:20 +0000

    Create about app

---
+++ about/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"Title {self.title}"
